d2a95ef843dfcd63813ae3d34f7f4728588bc7c4
Olaleru Gbolahan <74080732+klathazine9@users.noreply.github.com>
2021-08-18 07:49:32 +0000

    Update wsgi.py

--- klatapp/klatapp/wsgi.py
+++ klatapp/klatapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'klatapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weath.settings')
 
 application = get_wsgi_application()
